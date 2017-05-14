21af87fdc344ccb59205e1559e6599c5fec199d8
Man4ester <byrkovskiy@gmail.com>
2017-05-15 00:53:33 +0300

    init code

---
+++ src/main/java/com/bismark/functionalop/Main.java
@@ -0,0 +1,14 @@
+package com.bismark.functionalop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+	public int test() {
+		int count=0;
+		++count;
+		return count;
+	}
+}
